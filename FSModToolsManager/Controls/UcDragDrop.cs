0d9a1c681f3d8c1dcbdddfa3d38999598e177a51
Jiiks <stukov.alexei.va@gmail.com>
2024-07-14 08:19:42 +0300

    Drag&Drop control

---
+++ FSModToolsManager/Controls/UcDragDrop.cs
@@ -0,0 +1,38 @@
+﻿using FSModToolsManager.Core;
+using System.Diagnostics;
+
+namespace FSModToolsManager.Controls;
+internal class UcDragDrop : GroupBox {
+
+    public UcDragDrop(Tool tool) {
+        Text = tool.Name;
+
+        //Absolute size for now
+        Width = 50;
+        Height = 50;
+
+        var ddHandler = new Panel() {
+            AllowDrop = true,
+            Dock = DockStyle.Fill
+        };
+
+        ddHandler.DragEnter += DdHandler_DragEnter;
+        ddHandler.DragLeave += DdHandler_DragLeave;
+        ddHandler.DragDrop += DdHandler_DragDrop;
+
+        Controls.Add(ddHandler);
+    }
+
+    private void DdHandler_DragDrop(object? sender, DragEventArgs e) {
+        var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+        foreach (var file in files) Debug.WriteLine(file);
+    }
+
+    private void DdHandler_DragLeave(object? sender, EventArgs e) {
+        
+    }
+
+    private void DdHandler_DragEnter(object? sender, DragEventArgs e) {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+    }
+}
